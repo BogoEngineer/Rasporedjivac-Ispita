b57f226e3c375f71a0e0f41a7b5e98d8c2b3bd63
BogoEngineer <miloskat@gmail.com>
2020-12-21 13:21:24 +0100

    almost finished, needs testing

--- Classes.py
+++ Classes.py
@@ -1,24 +1,46 @@
+import sys
+import copy
+
+mapaTermina = {
+    0: "8:00",
+    1: "11:30",
+    2: "15:00",
+    3: "18:30"
+}
 
 class Sala:
-    def __init__(self, naziv, kapacitet, racunari, dezurni, etf):
+    def __init__(self, naziv, kapacitet, racunari, dezurni, etf, termin):
         self.naziv = naziv
         self.kapacitet = kapacitet
         self.racunari = (racunari == 1)
         self.dezurni = dezurni
-        self.etf = (etf==1)
+        self.etf = (etf == 1)
+        self.termin = termin
 
     def __str__(self):
-        return "naziv: "+self.naziv+" kapacitet: "+str(self.kapacitet)+" racunari:  "+str(self.racunari)+" dezurni: "+str(self.dezurni)+" etf:"+str(self.etf)
+        return "naziv: " + self.naziv + \
+               " kapacitet: " + str(self.kapacitet) + \
+               " racunari:  " + str(self.racunari) + \
+               " dezurni: " + str(self.dezurni) + \
+               " etf: " + str(self.etf) + \
+               " termin: " + str(mapaTermina[self.termin])
 
     def __repr__(self):
         return str(self)
+
+    def __eq__(self, other):
+        return self.naziv == other.naziv and self.termin == other.termin
+
+
 class Rok:
     def __init__(self, trajanje_u_danima, ispiti):
         self.trajanje_u_danima = trajanje_u_danima
         self.ispiti = ispiti
 
     def __str__(self):
-        return "trajanje: "+str(self.trajanje_u_danima)+"ispiti: "+str(self.ispiti)
+        return "trajanje: " + str(self.trajanje_u_danima) \
+               + "ispiti: " + str(self.ispiti)
+
 
 class Ispit:
     def __init__(self, sifra, prijavljeni, racunari, odseci):
@@ -28,7 +50,78 @@ class Ispit:
         self.odseci = odseci
 
     def __str__(self):
-        return "sifra: "+self.sifra+" prijavljeni: "+str(self.prijavljeni)+" racunari: "+str(self.racunari)+" odseci: "+str(self.odseci)
+        return "sifra: " + self.sifra + \
+               " prijavljeni: " + str(self.prijavljeni) + \
+               " racunari: " + str(self.racunari) + \
+               " odseci: " + str(self.odseci)
 
     def __repr__(self):
         return str(self)
+
+    def __eq__(self, other):
+        return other.sifra == self.sifra
+
+
+class ElementDomena:
+    def __init__(self, sifra_predmeta, sale):
+        self.sifra_predmeta = sifra_predmeta
+        self.dodeljeno = False
+        self.sale = sale  # predstavlja sale koje su u opticaju ako je dodeljeno == False, u suprotnom odabrane sale
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "Sifra_predmeta: "+self.sifra_predmeta+\
+               " Dodeljeno: "+str(self.dodeljeno)+\
+               " Sale: "+str(self.sale)
+
+class Stanje:
+    rezultat = None
+    min_poeni = sys.maxsize * 2 + 1  # max_int
+    ispiti = []
+
+    def __init__(self, domen):
+        self.domen = domen
+
+    def __str__(self):
+        return "Domen: "+str(self.domen)
+
+    def backtrack(self, trenutni_poeni, termin):
+        sledeca_promenljiva = min([x for x in self.domen if not x.dodeljeno], key=lambda d: len(d.sale))
+        if len(sledeca_promenljiva.sale) == 0: return
+        sledeci_ispit = next((x for x in Stanje.ispiti if x.sifra == sledeca_promenljiva.sifra_predmeta), None)
+        smesteni_stuednti = 0
+        temp_sale = []
+        while smesteni_stuednti < sledeci_ispit.prijavljeni:
+            if len(sledeca_promenljiva.sale) == 0: return
+            sledeca_sala = next((sala for sala in sledeca_promenljiva.sale if sala.termin == termin), None)  # sale su
+            # sortirane prema broju dezurnih i
+            # tome da li su na etfu ili ne, respektivno
+            if sledeca_sala is None: return
+            sledeca_promenljiva.sale.remove(sledeca_sala)
+            smesteni_stuednti += sledeca_sala.kapacitet
+            temp_sale.append(sledeca_sala)
+
+        sledeca_promenljiva.dodeljeno = True
+        sledeca_promenljiva.sale = temp_sale
+        for alocirana_sala in sledeca_promenljiva.sale:
+            trenutni_poeni += alocirana_sala.dezurni + (1.2 if alocirana_sala.etf == 0 else 0)
+
+        self.forward_check(temp_sale)
+
+        if trenutni_poeni >= Stanje.min_poeni: return
+
+        if len([x for x in self.domen if not x.dodeljeno]) == 0:
+            Stanje.min_poeni = trenutni_poeni if trenutni_poeni < Stanje.min_poeni else Stanje.min_poeni
+            Stanje.rezultat = self.domen
+            return
+
+        for i in range(4):
+            Stanje(copy.deepcopy(self.domen)).backtrack(trenutni_poeni, i)
+
+    def forward_check(self, uzete_sale):
+        for elem in self.domen:
+            if elem.dodeljeno: continue
+            for sala in uzete_sale:
+                if sala in elem.sale: elem.sale.remove(sala)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import copy
 
 from Classes import *
 
@@ -8,17 +9,54 @@ def main():
     """sale_file = input("Unesite ime .json fajla sa salama: ")
     rok_file = input("Unesite ime .json fajla sa rokom: ")"""
 
-    with open("javni_testovi\sale1.json", encoding="utf8", errors='ignore') as f:
+    with open("javni_testovi\sale3.json", encoding="utf8", errors='ignore') as f:
         sale_json = json.load(f)
 
-    with open("javni_testovi" +'\\' "rok1.json", encoding="utf8", errors='ignore') as f:
+    with open("javni_testovi" + '\\' "rok3.json", encoding="utf8", errors='ignore') as f:
         rok_json = json.load(f)
 
-    sale = [Sala(x['naziv'], x['kapacitet'], x['racunari'], x['dezurni'], x['etf']) for x in sale_json]
+    sale = [Sala(x['naziv'], x['kapacitet'], x['racunari'], x['dezurni'], x['etf'], i) for x in sale_json for i in
+            range(4)]
     rok = Rok(rok_json['trajanje_u_danima'],
               [Ispit(x['sifra'], x['prijavljeni'], x['racunari'], x['odseci']) for x in rok_json['ispiti']])
-    print(sale)
-    print(rok)
+
+    stanje = pocetnoStanje(rok, sale)
+
+    for i in range(4):
+        stanje.backtrack(0, i)
+
+    #print(Stanje.rezultat)
+    print("MIN POENI", Stanje.min_poeni)
+
+    with open('raspored.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Dan1'] + ['sala' + sala.naziv for sala in sale if sala.termin == 0])
+        for i in range(4):
+            novi_red = []
+            for _, sala in enumerate(sale):
+                if sala.termin != i: continue
+                to_append = [x for x in Stanje.rezultat if sala in x.sale]
+                # print("SALA: ", sala.naziv + " " + mapaTermina[sala.termin])
+                # print("TO APPEND: ", to_append)
+                to_append = to_append[0].sifra_predmeta if len(to_append) == 1 else 'X'
+                novi_red.append(to_append)
+            writer.writerow(['T' + str(i + 1)] + novi_red)
+
+
+def pocetnoStanje(rok, sale):
+    Stanje.ispiti = [x for x in rok.ispiti]
+
+    prosecan_kapacitet = 30 # utvrdjeno na osnovu javnih testova
+    loss_function = lambda x: -((x.dezurni*x.kapacitet)/prosecan_kapacitet + (1.2 if not x.etf else 0))
+    pocetni_domen = []
+    for ispit in rok.ispiti:
+        srtd = sorted([sala for sala in sale if ispit.racunari == sala.racunari],
+                      key=loss_function) # sortiraj prema najmanjem skoru
+        # print("SRTD: ", srtd)
+        pocetni_domen.append(ElementDomena(ispit.sifra, srtd))
+
+    return Stanje(pocetni_domen)
 
 
 if __name__ == "__main__":
